01d07fdf44ada1b8cc7c92673b0f1ea5cac95ff1
Cholpon Medetkulova <87806846+Cholpon88@users.noreply.github.com>
2022-01-28 16:36:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
-// Описание задания №2
-//     1. Дана строка **'ddd@bbb@ccc'**. Замените все **@** на **'!'**
-//     2. Дана строка **'js'**. Сделайте из нее строку **'JS'**.
-//     3. Дана строка **'JS'**. Сделайте из нее строку **'js'**.
-//     4. Дана строка**'I am a hero!'**. Выведите в консоль **количество символов** в этой строке.
-//     5. Составить скрипт, который запрашивает у пользователя ввести номер кредитной карты. Скрипт должен обработать полученный номер и показать на странице последние 4 цифры а остальные цифры карты должны заменены на *.
-//   
-//   **Пример: "Номер вашей карты: **********1245"**
+/**
+ * 1. Дана строка **'ddd@bbb@ccc'**. Замените все **@** на **'!'**
+2. Дана строка **'js'**. Сделайте из нее строку **'JS'**.
+3. Дана строка **'JS'**. Сделайте из нее строку **'js'**.
+4. Дана строка**'I am a hero!'**. Выведите в консоль **количество символов** в этой строке.
+5. Составить скрипт, который запрашивает у пользователя ввести номер кредитной карты. Скрипт должен обработать полученный номер и показать на странице последние 4 цифры а остальные цифры карты должны заменены на *.
+    
+    **Пример: "Номер вашей карты: **********1245"**
+ */
+let stM = 'ddd@bbb@ccc'
+let st = ''
+for (let i = 0; i < stM.length; i++) {
+    st += stM[i] === '@' ? '!' : stM[i];
+}
+console.log(st)
 
-//№1.
-document.write("<strong>Задача №2/1 </strong><br>");
-let text = "ddd@bbb@ccc";
-let newText = text.replaceAll('@', '!');
-console.log(newText);
 
 
-//№2.
-// document.write("<strong>Задача №2/2 </strong><br>");
-// const upperCaseText = 'js';
-// console.log(upperCaseText.toUpperCase());
+console.log('js-' + ('js'.toUpperCase()));
 
-//№3.
-// document.write("<strong>Задача №2/3 </strong><br>");
-// console.log('JS'.toLowerCase());
-// 
-// 
-// №4.
-// document.write("<strong>Задача №2/4 </strong><br>");
-// const numberSymbol = 'I am a hero!';
-// console.log(numberSymbol.length);
-// 
-// №5.
-// document.write("<strong>Задача №2/5 </strong><br>");
-// let numCard = prompt("Номер вашей карты: (16)");
-// let myCard = ("************+numCard.slice(12)");
-// document.write('"Номер вашей карты: **********1245"');
+
+console.log('JS-' + ('JS'.toLowerCase()));
+
+
+
+let stM2 = 'I am a hero!';
+let sim = 0;
+for (let i = 0; i < stM2.length; i++) {
+    sim++;
+}
+console.log(stM2, '- количество символов : ', sim);
+
+
+
+let card = (prompt('введите номер карты: '));
+let part = card.slice(0, card.length - 4);
+alert(card.replace(`${part}`, "****"))
