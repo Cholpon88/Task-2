d5fd1a30ba280e5288345a66403251bbe5263bd4
Cholpon Medetkulova <87806846+Cholpon88@users.noreply.github.com>
2022-01-29 11:33:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-/**
- * 1. Дана строка **'ddd@bbb@ccc'**. Замените все **@** на **'!'**
-2. Дана строка **'js'**. Сделайте из нее строку **'JS'**.
-3. Дана строка **'JS'**. Сделайте из нее строку **'js'**.
-4. Дана строка**'I am a hero!'**. Выведите в консоль **количество символов** в этой строке.
-5. Составить скрипт, который запрашивает у пользователя ввести номер кредитной карты. Скрипт должен обработать полученный номер и показать на странице последние 4 цифры а остальные цифры карты должны заменены на *.
+//1. Дана строка 'ddd@bbb@ccc'. Замените все @ на '!'
+let str = "ddd@bbb@ccc";
+document.write(str.replaceAll("@", "!") + "<br>");
+
+// 2. Дана строка 'js'. Сделайте из нее строку 'JS'.
+let str2 = "js";
+document.write(str2.toUpperCase() + "<br>");
+// 3. Дана строка 'JS'. Сделайте из нее строку 'js'.
+let str3 = "JS";
+document.write(str3.toLowerCase() + "<br>");
+// 4. Дана строка 'I am a hero!'. Выведите в консоль количество символов в этой строке.
+let str4 = "I am a hero!";
+document.write(str4.length + "<br>");
+
+/*5. Составить скрипт, который запрашивает у пользователя ввести номер кредитной карты. 
+Скрипт должен обработать полученный номер и показать на странице последние 4 цифры 
+а остальные цифры карты должны заменены на *.
     
-    **Пример: "Номер вашей карты: **********1245"**
- */
-let stM = 'ddd@bbb@ccc'
-let st = ''
-for (let i = 0; i < stM.length; i++) {
-    st += stM[i] === '@' ? '!' : stM[i];
-}
-console.log(st)
+    Пример: "Номер вашей карты: **********1245" */
 
+const symb = "*";
+let userCard = prompt("Введите номер карты");
+let finishNumberCard = prompt("Сколько последних чисел вам показать? ");
+let NonVisNum = userCard.length - finishNumberCard;
+let NonVisCardNum = userCard.slice(0, NonVisNum);
+let NonVisCardLenght = NonVisCardNum.length;
+let nonVisReplace = NonVisCardNum.replace(
+  userCard.slice(0, NonVisNum),
+  symb.repeat(NonVisCardLenght)
+);
 
-
-console.log('js-' + ('js'.toUpperCase()));
-
-
-console.log('JS-' + ('JS'.toLowerCase()));
-
-
-
-let stM2 = 'I am a hero!';
-let sim = 0;
-for (let i = 0; i < stM2.length; i++) {
-    sim++;
-}
-console.log(stM2, '- количество символов : ', sim);
-
-
-
-let card = (prompt('введите номер карты: '));
-let part = card.slice(0, card.length - 4);
-alert(card.replace(`${part}`, "****"))
+document.write(
+  `Показываем последние ${finishNumberCard} цифр(ы) вашей карты: ${nonVisReplace}${userCard.slice(
+    userCard.length - finishNumberCard,
+    userCard.length
+  )}`
+);
